a629ba05176b9d774cd68d6bac5ee88d8e4f82e9
DarrenSweeney <sweeney1995@live.co.uk>
2015-09-24 19:13:01 +0100

    Adjusted print statements.

--- CramersRule/main.cpp
+++ CramersRule/main.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 /*
-	Darren Sweeney.
-	Demonstrating Cramer's Rule.
+Darren Sweeney.
+Demonstrating Cramer's Rule.
 
-	Order of matrix representation:
-			0 1 2
-			3 4 5
-			6 7 8
+Order of matrix representation:
+0 1 2
+3 4 5
+6 7 8
 */
 
 // Note: Uses the + - + sign pattern for the terms in the expansion.
@@ -33,8 +33,9 @@ void main(int argc, char* argv[])
 		cin >> element;
 		matrix[i] = element;
 	}
+	cout << "\n";
 
-	int matrix_D[9]{matrix[0], matrix[1] , matrix[2],
+	int matrix_D[9]{ matrix[0], matrix[1] , matrix[2],
 		matrix[4] , matrix[5] , matrix[6] ,
 		matrix[8] , matrix[9] , matrix[10] };
 	int deter_D = determinateOfMatrix(matrix_D);
@@ -54,10 +55,10 @@ void main(int argc, char* argv[])
 		matrix[8] , matrix[9] , matrix[11] };
 	int deter_Dz = determinateOfMatrix(matrix_Dz);
 
-	cout << "deter_D " << deter_D << endl;
-	cout << "deter_Dx " << deter_Dx << endl;
-	cout << "deter_Dy " << deter_Dy << endl;
-	cout << "deter_Dz " << deter_Dz << endl;
+	cout << "determinate D = " << deter_D << endl;
+	cout << "determinate Dx = " << deter_Dx << endl;
+	cout << "determinate Dy = " << deter_Dy << endl;
+	cout << "determinate Dz = " << deter_Dz << "\n" << endl;
 
 	if (deter_D != 0)
 	{
@@ -68,6 +69,6 @@ void main(int argc, char* argv[])
 
 		cout << "Answere is (" << x << "," << y << "," << z << ")" << endl;
 	}
-	int i;
-	cin >> i;
+	else
+		cout << "Your equation can not be solved with Cramer's Rule\n" << endl;
 }
